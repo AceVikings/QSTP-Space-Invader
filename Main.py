7cb590331f9bfb8ed401dd89e3844ad47b31d21a
akashjha011 <51465755+akashjha011@users.noreply.github.com>
2019-07-18 12:45:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,8 +13,20 @@ clock = pygame.time.Clock()
 
 spaceS = pygame.image.load("ss.png")
 spaceS = pygame.transform.scale(spaceS,(80,80))
+enemy_5 = pygame.image.load('1.png')
+enemy_5 = pygame.transform.scale(enemy_5,(80,80))
+enemy_5 = pygame.transform.flip(enemy_5,False,True)
+enemy_10 = pygame.image.load('2.png')
+enemy_10 = pygame.transform.scale(enemy_10,(80,80))
+enemy_10 = pygame.transform.flip(enemy_10,False,True)
+enemy_15 = pygame.image.load('3.png')
+enemy_15 = pygame.transform.scale(enemy_15,(80,80))
+enemy_15 = pygame.transform.flip(enemy_15,False,True)
 background = pygame.image.load('bg1.jpeg')
-
+sprites = []    # List of sprites
+i_flip = False      # Flag to reverse invader direction
+i_count = 0         # Invader count
+i_miss_count = 0 # Invader missile count
 class spaceShip(object):
     def __init__(self, x, y, width, height):
         self.x = x
@@ -63,12 +75,22 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
+    #Making Enemy Spaceships appear
+
+
+
+    #bullet shoot
     if keys[pygame.K_SPACE]:
 
         if len(bullets) < 500:
             bullets.append(shots(round(ss.x + 40),round(ss.y +40),(128,128,128)))
-
-
+    #bullet collision check
+    for bullet in bullets:
+        for i in range(5,20,5):
+            if pygame.sprite.collide_rect(bullet,'enemy_'+str(i)):
+                bullet.kill()
+                'enemy_'+str(i).kill()
+    #movements
     if keys[pygame.K_LEFT]:
         if ss.x > 0:
             ss.x -= ss.vel_ss
